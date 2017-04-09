d7fd5ef170dadf31ef5f298d27833a7428eff9ba
gabyx08 <gaby.ranarz@gmail.com>
2017-04-09 13:07:38 -0500

    se muestra un alert cuando la serpiente toca el borde

--- javascript.js
+++ javascript.js
@@ -11,21 +11,33 @@ function moverSerpiente(event){
 	switch(tecla){
 		case 37: //izquierda
 		x = x-100;
+		if(x<0){
+			alert("borde");
+		}
 		serpiente.style.marginLeft = x + "px";
 		break;
 
 		case 38: //arriba
 		y = y-100;
+		if(y<0){
+			alert("borde");
+		}
 		serpiente.style.marginTop = y + "px";
 		break;
 
 		case 39: //derecha
 		x = x+100;
+		if(x>400){
+			alert("borde");
+		}
 		serpiente.style.marginLeft = x + "px";
 		break;
 
 		case 40: //abajo
 		y = y+100;
+		if(y>400){
+			alert("borde");
+		}
 		serpiente.style.marginTop = y + "px";
 		break;
 
